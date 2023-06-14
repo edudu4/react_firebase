86cb178eb7690667fea99c75bf20696c0e1e70c4
unknown <2014290030@ACADEMICO.IESB.BR>
2023-06-13 22:13:58 -0300

    CRUD com FireBase

--- src/pages/HomeList.jsx
+++ src/pages/HomeList.jsx
@@ -1,22 +1,42 @@
-import { useEffect } from "react"
-import { listaTarefas } from '../services/TaskService'
+import { useEffect, useState } from "react"
+import { useNavigate } from "react-router-dom"
+import { listaTarefas, removeTarefa } from '../services/TaskService'
 
 export default function HomeList() {
-  const tarefas = []
+  const [tarefas, setTarefas] = useState([])
+  const [loading, setLoading] = useState(false)
+  const navigate = useNavigate()
 
-  useEffect(()=> {
-    async function carrega () {
-      await listaTarefas()
-    }
+  async function carrega() {
+    setLoading(true)
+    const data = await listaTarefas()
+    setTarefas(data)
+    setLoading(false)
+  }
+  useEffect(() => {
     carrega()
   }, [])
 
+  function handleEditar(key) {
+    navigate("/editar/" + key)
+  }
+
+  async function handleRemover(key) {
+    await removeTarefa(key)
+    carrega()
+  }
+
   return (
     <>
-    <ol>
-      {tarefas.map((tarefa, key) => 
-      <li key={key}>{tarefa.nome} - {tarefa.prioridade}</li>)}
-    </ol>
+      {loading ? <h3>Aguarde...</h3> :
+        <ol>
+          {tarefas.map((tarefa, key) =>
+            <li key={key}>{tarefa.nome} - {tarefa.prioridade}
+            <button onClick={() => handleEditar(tarefa.key)}>Editar</button>
+            <button onClick={() => handleRemover(tarefa.key)}>Remover</button>
+            </li>)}
+        </ol>
+      }
     </>
   )
 }
--- src/services/TaskService.js
+++ src/services/TaskService.js
@@ -1,23 +1,52 @@
 import { urlApi } from "./FirebaseConfig"
+
+//GET listar
 async function listaTarefas() {
-   await fetch(urlApi)
-   .then((response) => response.json())
-   .then((data) => console.log(data))
-   .catch((error) => {throw Error("Deu ruim")})
+    let tarefas = []
+    await fetch(urlApi + "/tarefas.json")
+        .then((response) => response.json())
+        .then((data) => {
+            for (let key in data) {
+                tarefas.push({ key, ...data[key] })
+            }
+        })
+        .catch((error) => { throw Error("Deu ruim") })
+    return tarefas
 }
 
+//POST criar, inserir
 async function insereTarefa(tarefa) {
-    await fetch(urlApi, {
+    await fetch(urlApi + "tarefas.json", {
         method: 'POST',
         body: JSON.stringify(tarefa),
+        headers: {
+            'Content-type': 'application/json'
+        }
+    })
+        .catch((error) => { throw Error("Deu ruim") })
+}
+
+
+//PUT alterar, modificar
+async function modificaTarefa(tarefa) {
+    await fetch(urlApi + "tarefas/" + tarefa.key + ".json", {
+        method: 'PUT',
+        body: JSON.stringify({nome : tarefa.nome, prioridade : tarefa.prioridade}),
         headers: {
             'Content-type' : 'application/json'
         }
     })
-    .catch((error) => {throw Error("Deu ruim")})
 }
 
-export {listaTarefas, insereTarefa }
+//DELETE destruir, remover
+async function removeTarefa(key) {
+    await fetch(urlApi + "tarefas/" + key + ".json", {
+        method: 'DELETE'
+    })
+    .catch((error) => {throw Error("Deu Ruim")})
+}
+
+export { listaTarefas, insereTarefa, modificaTarefa, removeTarefa }
 
 //Obj JS
 //{ nome: "Fazer algo", prioridade: 1}
